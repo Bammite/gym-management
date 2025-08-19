9604ab31e7b8352987470a8406e5d1d519844ba1
bammite_prince <bammite_prince>
2025-08-19 14:36:37 +0000

    customisation

---
+++ code/js/ElementEntreprise.js
@@ -0,0 +1,5 @@
+const linksLogo = './assets/images/logoAmbyFitness_removebg-minimaliste.png';
+const nomEntreprise = 'Amby Fitness';
+const contactEntreprise = '+221781941351';
+const emailEntreprise = 'contact@ambyfitness.com';
+const adresseEntreprise = 'Mamelles, Dakar, Senegal';
